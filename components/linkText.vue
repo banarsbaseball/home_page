22a3f800b87d01f03c5b21c8bbd25d8469d25262
kerokerotur <keigen2015@gmail.com>
2022-10-26 21:06:31 +0900

    [BHP-3]画面遷移に使用するボタンを共通コンポーネントとして作成する feat: ページ遷移用の共通コンポーネントを作成 feat: 選手紹介、試合日程ページのひな型作成 feat: nuxt-property-decoratorをインストール

---
+++ components/linkText.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .div
+    a#link(@click=`transitionPage`) {{buttonTitle}}
+    
+</template>
+
+<script lang="ts">
+  import { Vue, Prop, Component} from "nuxt-property-decorator"
+
+  @Component({
+  name: "linkText",
+})
+
+  export default class extends Vue {
+    @Prop({ default: ""}) toPagePath!: string
+    @Prop({ default: ""}) buttonTitle!: string
+
+    private transitionPage(): void {
+      this.$router.push(`${this.toPagePath}`)
+    }
+  }
+</script>
+
+<style>
+#link {
+  font-weight: 900;
+  font-size: 150%;
+  color: black;
+  padding: 20px;
+  transition: all 0.3s ease;
+}
+
+#link:hover {
+  color: deeppink;
+  font-size: 200%;
+  transition: all 0.3s ease;
+}
+
+</style>
---
+++ pages/schedule/index.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.div
+</template>
+
+<script lang="ts">
+import { Component, Vue} from "nuxt-property-decorator"
+
+@Component({
+  name: "schedule"
+})
+
+export default class extends Vue{
+
+}
+</script>
+
+<style>
+  .schedule {
+    background-size: cover;
+    background-position: center;
+    background-image: url(../../static/image/schedule.jpg);
+    min-height: 100vh;
+  }
+</style>
